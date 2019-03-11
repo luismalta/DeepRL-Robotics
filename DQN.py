77d9d4abfc1772d431475d9d86b56df00c4cf86a
Luis Malta <luiso.malta@gmail.com>
2019-03-11 11:35:23 -0300

    [FIX]: Code reformulation

--- DQN.py
+++ DQN.py
@@ -12,31 +12,27 @@ import numpy as np
 import gym
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
+from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
 import keras
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
-from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 import os.path
+import argparse
+import sys
 
+parser = argparse.ArgumentParser(description='Type of run: Train or Test')
+parser.add_argument('-t', '--type', help='Type of run: Train or Test', required=True)
+args = parser.parse_args()
 
-if __name__ == "__main__":
-    env = gym.make('GazeboCircuit2TurtlebotLidarNn-v0')
-    outdir = '/tmp/gazebo_gym_experiments/'
-    env = wrappers.Monitor(env, '/tmp/{}'.format('teste'), force=True)
-
-    np.random.seed(123)
-    env.seed(123)
-    nb_actions = 21
-
-    # Next, we build a very simple model.
+#Build the model for the agent
+def build_model():
     model = Sequential()
-    model.add(Dense(300,input_shape=[1,100]))
+    model.add(Dense(300, input_shape=[1, 100]))
     model.add(Activation('linear'))
     model.add(Flatten())
     model.add(Dense(300))
@@ -46,31 +42,67 @@ if __name__ == "__main__":
     model.add(Dense(300))
     model.add(Activation('relu'))
     model.add(Dense(nb_actions))
-
     model.add(Activation('linear'))
     print(model.summary())
 
-    tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs/DQN', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
+    return model
 
-    tbCallBack.set_model(model)
-    # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
-    # even the metrics!
-    memory = SequentialMemory(limit=50000, window_length=1)
-    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=1000000)
-    dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-                nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-               train_interval=4, delta_clip=1.)
-    dqn.compile(Adam(lr=.00025), metrics=['mae'])
-
-    if (os.path.isfile('/home/luismalta/Projects/DeepRL/models//home/luismalta/Projects/DeepRL/models/drl_GazeboCircuit2cTurtlebotCameraNnEnv-v0_DQN_weights.h5f.h5f')):
-        print('Loading previous model...')
-        dqn.load_weights('/home/luismalta/Projects/DeepRL/models//home/luismalta/Projects/DeepRL/models/drl_GazeboCircuit2cTurtlebotCameraNnEnv-v0_DQN_weights.h5f.h5f')
-
-    # Okay, now it's time to learn something! We visualize the training here for show, but this
-    # slows down training quite a lot. You can always safely abort the training prematurely using
-    # Ctrl + C.
+#Train the model
+def train():
+    print("Training model...")
     dqn.fit(env, nb_steps=1000000, visualize=False, verbose=2, callbacks=[tbCallBack])
 
-    # After training is done, we save the final weights.
-    dqn.save_weights('/home/luismalta/Projects/DeepRL/models/drl_{}_weights.h5f'.format('GazeboCircuit2cTurtlebotCameraNnEnv-v0_DQN'), overwrite=False)
+    dqn.save_weights('models/DQN/drl_{}_weights.h5f'.format('DQN'), overwrite=False)
+
+#Test the model
+def test():
+    print("Testing model...")
+    dqn.test(env, nb_episodes=100, visualize=False)
+
+
+if __name__ == "__main__":
+
+    if (args.type != "train" and args.type != "test"):
+        print('Invalid Option')
+        sys.exit()
+
+    #Env setup
+    env = gym.make('GazeboCircuit2TurtlebotLidarNn-v0')
+
+    outdir = '/tmp/gazebo_gym_experiments/'
+    env = wrappers.Monitor(env, '/tmp/{}'.format('teste'), force=True)
+
+    np.random.seed(123)
+    env.seed(123)
+    nb_actions = 21
+
+    model = build_model()
+
+    #Tensorboard callback
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs/DQN', histogram_freq=0,
+                                        batch_size=32, write_graph=True, write_grads=False,
+                                        write_images=True, embeddings_freq=0, embeddings_layer_names=None,
+                                        embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
+    tbCallBack.set_model(model)
+
+    #Agent configuration
+    memory = SequentialMemory(limit=50000, window_length=1)
+
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1.,
+                                value_min=.1, value_test=.05,nb_steps=1000000)
+
+    dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
+                    nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
+                    train_interval=4, delta_clip=1.)
+
+    dqn.compile(Adam(lr=.00025), metrics=['mae'])
+
+    #Check previous models
+    if (os.path.isfile('models/DQN/drl_DQN_weights.h5f')):
+        print('Loading previous model...')
+        dqn.load_weights('models/DQN/drl_DQN_weights.h5f')
+
+    if (args.type == "train"):
+        train()
+    elif (args.type == "test"):
+        test()
--- Double_DQN.py
+++ Double_DQN.py
@@ -12,58 +12,97 @@ import numpy as np
 import gym
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
+from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
 import keras
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
-from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 import os.path
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Type of run: Train or Test')
+parser.add_argument('-t', '--type', help='Type of run: Train or Test', required=True)
+args = parser.parse_args()
+
+#Build the model for the agent
+def build_model():
+    model = Sequential()
+    model.add(Dense(300, input_shape=[1, 100]))
+    model.add(Activation('linear'))
+    model.add(Flatten())
+    model.add(Dense(300))
+    model.add(Activation('relu'))
+    model.add(Dense(300))
+    model.add(Activation('relu'))
+    model.add(Dense(300))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
+
+    return model
+
+#Train the model
+def train():
+    print("Training model...")
+    dqn.fit(env, nb_steps=1000000, visualize=False, verbose=2, callbacks=[tbCallBack])
+
+    dqn.save_weights('models/Double_DQN/drl_{}_weights.h5f'.format('Double_DQN'), overwrite=False)
+
+#Test the model
+def test():
+    print("Testing model...")
+    dqn.test(env, nb_episodes=100, visualize=False)
 
 
 if __name__ == "__main__":
-    env = gym.make('GazeboCircuit2cTurtlebotCameraNnEnv-v0')
+
+    if (args.type != "train" and args.type != "test"):
+        print('Invalid Option')
+        sys.exit()
+
+    #Env setup
+    env = gym.make('GazeboCircuit2TurtlebotLidarNn-v0')
+
     outdir = '/tmp/gazebo_gym_experiments/'
     env = wrappers.Monitor(env, '/tmp/{}'.format('teste'), force=True)
 
     np.random.seed(123)
     env.seed(123)
-    nb_actions = 3
+    nb_actions = 21
 
+    model = build_model()
 
-    model = Sequential()
-    model.add(Flatten(input_shape=(1,) + (1,1,32,32)))
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(16))
-    model.add(Activation('relu'))
-    model.add(Dense(nb_actions))
-
-    model.add(Activation('linear'))
-    print(model.summary())
-
-    tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs/Double_DQN', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
-
+    #Tensorboard callback
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='./logs/Double_DQN', histogram_freq=0,
+                                        batch_size=32, write_graph=True, write_grads=False,
+                                        write_images=True, embeddings_freq=0, embeddings_layer_names=None,
+                                        embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
     tbCallBack.set_model(model)
 
+    #Agent configuration
     memory = SequentialMemory(limit=50000, window_length=1)
-    policy = BoltzmannQPolicy()
-    dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
-                   enable_double_dqn=True, target_model_update=1e-2, policy=policy)
-    dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
-    if (os.path.isfile('/home/luismalta/Projects/DeepRL/models/drl_GazeboCircuit2cTurtlebotCameraNnEnv-v0_Double_DQN_weights.h5f')):
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1.,
+                                value_min=.1, value_test=.05,nb_steps=1000000)
+
+    dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
+                    nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
+                    train_interval=4, delta_clip=1., enable_double_dqn=True)
+
+    dqn.compile(Adam(lr=.00025), metrics=['mae'])
+
+    #Check previous models
+    if (os.path.isfile('models/Double_DQN/drl_Double_DQN_weights.h5f')):
         print('Loading previous model...')
-        dqn.load_weights('/home/luismalta/Projects/DeepRL/models/drl_GazeboCircuit2cTurtlebotCameraNnEnv-v0_Double_DQN_weights.h5f')
+        dqn.load_weights('models/Double_DQN/drl_Double_DQN_weights.h5f')
 
-
-    dqn.fit(env, nb_steps=200000, visualize=False, verbose=2, callbacks=[tbCallBack])
-
-
-    dqn.save_weights('/home/luismalta/Projects/DeepRL/models/drl_{}_weights.h5f'.format('GazeboCircuit2cTurtlebotCameraNnEnv-v0_Double_DQN'), overwrite=True)
+    if (args.type == "train"):
+        train()
+    elif (args.type == "test"):
+        test()
