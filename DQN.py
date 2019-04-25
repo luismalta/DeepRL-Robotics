cda492d1bb8aee74b024970649fd0ac79a470d0e
Luis Malta <luiso.malta@gmail.com>
2019-04-25 11:29:04 -0300

    [FIX]JSON reformulation and delete continuous

--- DQN.py
+++ DQN.py
@@ -16,12 +16,16 @@ from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
 import keras
 
+import json
+
 from rl.agents.dqn import DQNAgent
 from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 import os.path
+import os
+import errno
 import argparse
 import sys
 
@@ -33,15 +37,47 @@ parser.add_argument('--train', action='store_true', help='Train agent')
 parser.add_argument('--test', action='store_true', help='Test agent')
 args = parser.parse_args()
 
-nb_steps = 10
-nb_episodes = 100
-log_dir = './logs/DQN/log_{}'.format(datetime.now().strftime('%d-%m-%Y_%H:%M'))
+params = {
+    'train_test' : {
+        'nb_steps': 1000000,
+        'nb_episodes_test' : 100,
+        'time_start': datetime.now().strftime('%d-%m-%Y_%H:%M')
+    },
+    'agent' : {
+        'nb_steps_warmup':5000,
+        'gamma':.99,
+        'target_model_update':10,
+        'train_interval':4,
+        'delta_clip':1
+    },
+    'police':{
+        'exploretion_value_max':1.,
+        'exploretion_value_min':.1,
+        'exploretion_value_test':.05,
+        'exploration_nb_steps':250000
+    },
+    'compile':{
+        'learn_rate':.001,
+        'metrics':['mae']
+    }
+}
+
+
+log_dir = './logs/DQN/log_{}'.format(params['train_test']['time_start'])
+import distutils.dir_util
+distutils.dir_util.mkpath(log_dir)
 
 if(args.test == True and args.weight == None):
     print("Provide the weight file name")
     sys.exit()
 else:
-    weight_dir = 'models/DQN/{}.h5f'.format(args.weight)
+    weight_dir = args.weight
+
+with open(log_dir +'/params.json', 'w') as fp:
+    json.dump(params, fp)
+
+
+
 
 #Build the model for the agent
 def build_model():
@@ -64,16 +100,22 @@ def build_model():
 #Train the model
 def train():
     print("Training model...")
-    dqn.fit(env, nb_steps=nb_steps, visualize=False, verbose=2, callbacks=[tbCallBack])
+    train_history = dqn.fit(env, nb_steps=params['train_test']['nb_steps'], visualize=False, verbose=2, callbacks=[tbCallBack])
+    print(train_history)
 
-    dqn.save_weights('./models/DQN/{}_steps_{}_weights.h5f'.format(nb_steps,datetime.now().strftime('%d-%m-%Y_%H:%M')), overwrite=False)
+    with open(log_dir + '/train_history.json', 'w') as fp:
+        json.dump(train_history.history, fp)
+
+    dqn.save_weights('{}/{}_weights.h5f'.format(log_dir,params['train_test']['time_start']), overwrite=False)
 
 
 #Test the model
 def test():
     print("Testing model...")
-    dqn.test(env, nb_episodes=nb_episodes, visualize=False)
+    test_history = dqn.test(env, nb_episodes=params['train_test']['nb_episodes_test'], visualize=False)
 
+    with open(log_dir + '/test_history.json', 'w') as fp:
+        json.dump(test_history.history, fp)
 
 if __name__ == "__main__":
 
@@ -103,14 +145,15 @@ if __name__ == "__main__":
     #Agent configuration
     memory = SequentialMemory(limit=50000, window_length=1)
 
-    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1.,
-                                value_min=.1, value_test=.05,nb_steps=1000000)
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=params['police']['exploretion_value_max'],
+                                value_min=params['police']['exploretion_value_min'], value_test=params['police']['exploretion_value_test'],
+                                nb_steps=params['police']['exploration_nb_steps'])
 
     dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-                    nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-                    train_interval=4, delta_clip=1.)
+                    nb_steps_warmup=params['agent']['nb_steps_warmup'], target_model_update=params['agent']['target_model_update'],
+                    train_interval=params['agent']['train_interval'], delta_clip=params['agent']['delta_clip'],gamma=params['agent']['gamma'])
 
-    dqn.compile(Adam(lr=.00025), metrics=['mae'])
+    dqn.compile(Adam(lr=params['compile']['learn_rate']), metrics=params['compile']['metrics'])
 
     #Check previous models
     if (os.path.isfile(weight_dir)):
--- Dueling_DQN.py
+++ Dueling_DQN.py
@@ -16,33 +16,67 @@ from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
 import keras
 
+import json
+
 from rl.agents.dqn import DQNAgent
 from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 import os.path
+import os
+import errno
 import argparse
 import sys
 
 from datetime import datetime
 
-parser = argparse.ArgumentParser(description='Dueling DQN agent')
+parser = argparse.ArgumentParser(description='DQN agent')
 parser.add_argument('-w', '--weight', help='Weight file name',default=None)
 parser.add_argument('--train', action='store_true', help='Train agent')
 parser.add_argument('--test', action='store_true', help='Test agent')
 args = parser.parse_args()
 
+params = {
+    'train_test' : {
+        'nb_steps': 1000000,
+        'nb_episodes_test' : 100,
+        'time_start': datetime.now().strftime('%d-%m-%Y_%H:%M')
+    },
+    'agent' : {
+        'nb_steps_warmup':5000,
+        'gamma':.99,
+        'target_model_update':10,
+        'train_interval':4,
+        'delta_clip':1,
+        'dueling_type':'avg'
+    },
+    'police':{
+        'exploretion_value_max':1.,
+        'exploretion_value_min':.1,
+        'exploretion_value_test':.05,
+        'exploration_nb_steps':250000
+    },
+    'compile':{
+        'learn_rate':.001,
+        'metrics':['mae']
+    }
+}
 
-nb_steps = 10
-nb_episodes = 100
-log_dir = './logs/Dueling_DQN/log_{}'.format(datetime.now().strftime('%d-%m-%Y_%H:%M'))
+
+log_dir = './logs/Dueling_DQN/log_{}'.format(params['train_test']['time_start'])
+import distutils.dir_util
+distutils.dir_util.mkpath(log_dir)
 
 if(args.test == True and args.weight == None):
     print("Provide the weight file name")
     sys.exit()
 else:
     weight_dir = 'models/Dueling_DQN/{}.h5f'.format(args.weight)
+
+with open(log_dir +'/params.json', 'w') as fp:
+    json.dump(params, fp)
+
 
 
 
@@ -67,15 +101,22 @@ def build_model():
 #Train the model
 def train():
     print("Training model...")
-    dqn.fit(env, nb_steps=nb_steps, visualize=False, verbose=2, callbacks=[tbCallBack])
+    train_history = dqn.fit(env, nb_steps=params['train_test']['nb_steps'], visualize=False, verbose=2, callbacks=[tbCallBack])
+    print(train_history)
 
-    dqn.save_weights('./models/Dueling_DQN/{}_steps_{}_weights.h5f'.format(nb_steps,datetime.now().strftime('%d-%m-%Y_%H:%M')), overwrite=False)
+    with open(log_dir + '/train_history.json', 'w') as fp:
+        json.dump(train_history.history, fp)
+
+    dqn.save_weights('{}/{}_weights.h5f'.format(log_dir,params['train_test']['time_start']), overwrite=False)
+
 
 #Test the model
 def test():
     print("Testing model...")
-    dqn.test(env, nb_episodes=nb_episodes, visualize=False)
+    test_history = dqn.test(env, nb_episodes=params['train_test']['nb_episodes_test'], visualize=False)
 
+    with open(log_dir + '/test_history.json', 'w') as fp:
+        json.dump(test_history.history, fp)
 
 if __name__ == "__main__":
 
@@ -105,14 +146,16 @@ if __name__ == "__main__":
     #Agent configuration
     memory = SequentialMemory(limit=50000, window_length=1)
 
-    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1.,
-                                value_min=.1, value_test=.05,nb_steps=100000)
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=params['police']['exploretion_value_max'],
+                                value_min=params['police']['exploretion_value_min'], value_test=params['police']['exploretion_value_test'],
+                                nb_steps=params['police']['exploration_nb_steps'])
 
     dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-                    nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-                    train_interval=4, delta_clip=1., enable_dueling_network=True, dueling_type='avg')
+                    nb_steps_warmup=params['agent']['nb_steps_warmup'], target_model_update=params['agent']['target_model_update'],
+                    train_interval=params['agent']['train_interval'], delta_clip=params['agent']['delta_clip'],gamma=params['agent']['gamma'],
+                    dueling_type=params['agent']['dueling_type'], enable_dueling_network=True)
 
-    dqn.compile(Adam(lr=.00025), metrics=['mae'])
+    dqn.compile(Adam(lr=params['compile']['learn_rate']), metrics=params['compile']['metrics'])
 
     #Check previous models
     if (os.path.isfile(weight_dir)):
